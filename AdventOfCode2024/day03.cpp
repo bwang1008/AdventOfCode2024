735677fae148a65fd78b20cea9e1b150c6144899
bwang1008 <bwang1008@gmail.com>
2024-12-04 00:50:13 -0600

    Finish day03B

--- AdventOfCode2024/day03.cpp
+++ AdventOfCode2024/day03.cpp
@@ -36,7 +36,33 @@ auto solve_a() -> int64_t {
     return sum;
 }
 
-auto solve_b() -> int64_t { return 0; }
+auto solve_b() -> int64_t {
+    // see https://en.cppreference.com/w/cpp/regex
+    const std::regex mul_pattern(R"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))");
+    int64_t sum = 0;
+    bool enabled = true;
+
+    const std::string memory = parse_input();
+    auto mul_start =
+        std::sregex_iterator(memory.begin(), memory.end(), mul_pattern);
+    auto mul_end = std::sregex_iterator();
+
+    for(std::sregex_iterator i = mul_start; i != mul_end; ++i) {
+        std::smatch match = *i;
+
+        if(match[0] == "do()") {
+            enabled = true;
+        } else if(match[0] == "don't()") {
+            enabled = false;
+        }
+        if(match.str().substr(0, 3) == "mul" && enabled) {
+            const int left = std::stoi(match[1]);
+            const int right = std::stoi(match[2]);
+            sum += left * right;
+        }
+    }
+    return sum;
+}
 
 auto main(int argc, char *argv[]) -> int {
     const std::vector<std::string> args(argv, argv + argc);
