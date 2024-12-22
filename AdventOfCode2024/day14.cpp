3fee72b86034d209178fdf4b1624130479d86e70
bwang1008 <bwang1008@gmail.com>
2024-12-22 05:07:23 -0600

    Finish day14B

--- AdventOfCode2024/day14.cpp
+++ AdventOfCode2024/day14.cpp
@@ -26,9 +26,9 @@ class RestroomRobot {
         return std::pair(current_col_position, current_row_position);
     }
     auto step(const int num_seconds) -> void {
-        int final_col = (initial_col_position + num_seconds * col_velocity) %
+        int final_col = (current_col_position + num_seconds * col_velocity) %
                         BATHROOM_WIDTH;
-        int final_row = (initial_row_position + num_seconds * row_velocity) %
+        int final_row = (current_row_position + num_seconds * row_velocity) %
                         BATHROOM_HEIGHT;
         if(final_col < 0) {
             final_col += BATHROOM_WIDTH;
@@ -108,6 +108,63 @@ auto print_robot_positions(const std::vector<RestroomRobot> &robots) -> void {
     }
 }
 
+auto find_percentage_of_robot_positions_have_at_least_one_neighbor(
+    const std::vector<RestroomRobot> &robots) -> double {
+    std::vector<std::vector<bool>> visited(
+        BATHROOM_HEIGHT, std::vector<bool>(BATHROOM_WIDTH, false));
+    for(const auto robot : robots) {
+        const std::pair<int, int> current_position =
+            robot.get_current_position();
+        visited[static_cast<std::size_t>(current_position.second)]
+               [static_cast<std::size_t>(current_position.first)] = true;
+    }
+
+    int number_of_robot_positions = 0;
+    for(const auto &row : visited) {
+        for(const bool is_visited : row) {
+            if(is_visited) {
+                ++number_of_robot_positions;
+            }
+        }
+    }
+
+    const std::vector<int> dx{-1, -1, -1, 0, 0, 1, 1, 1};
+    const std::vector<int> dy{-1, 0, 1, -1, 1, -1, 0, 1};
+
+    int num_have_neighbors = 0;
+    for(std::size_t i = 0; i < visited.size(); ++i) {
+        for(std::size_t j = 0; j < visited[i].size(); ++j) {
+            if(!visited[i][j]) {
+                continue;
+            }
+
+            bool has_neighbors = false;
+            for(std::size_t k = 0; k < dx.size(); ++k) {
+                int dx1 = dx[k];
+                int dy1 = dy[k];
+
+                std::size_t neighbor_row =
+                    (i + static_cast<std::size_t>(BATHROOM_HEIGHT + dx1)) %
+                    BATHROOM_HEIGHT;
+                std::size_t neighbor_col =
+                    (j + static_cast<std::size_t>(BATHROOM_WIDTH + dy1)) %
+                    BATHROOM_WIDTH;
+
+                if(visited[neighbor_row][neighbor_col]) {
+                    has_neighbors = true;
+                    break;
+                }
+            }
+
+            if(has_neighbors) {
+                ++num_have_neighbors;
+            }
+        }
+    }
+
+    return num_have_neighbors / static_cast<double>(number_of_robot_positions);
+}
+
 auto solve_day14a() -> int64_t {
     constexpr const int NUM_SECONDS = 100;
     std::vector<RestroomRobot> restroom_robots = parse_input();
@@ -123,16 +180,38 @@ auto solve_day14a() -> int64_t {
 }
 
 auto solve_day14b() -> int64_t {
-    constexpr const int MAX_SECONDS_TO_TRY = 10;
+    constexpr const int VERIFIED_SECONDS = 0;
+    constexpr const int MAX_SECONDS_TO_TRY = 30000;
+    constexpr const double NEIGHBOR_TOLERANCE = 0.7;
+    constexpr const int LOGGING_FREQUENCY = 200;
+
     std::vector<RestroomRobot> restroom_robots = parse_input();
-    int num_seconds = 1;
+    for(RestroomRobot &restroom_robot : restroom_robots) {
+        restroom_robot.step(VERIFIED_SECONDS);
+    }
+
+    int num_seconds = 1 + VERIFIED_SECONDS;
     while(num_seconds < MAX_SECONDS_TO_TRY) {
+
+        if(num_seconds % LOGGING_FREQUENCY == 0) {
+            std::cout << "num_seconds at " << num_seconds << std::endl;
+        }
+
         for(RestroomRobot &restroom_robot : restroom_robots) {
             restroom_robot.step(1);
         }
-        std::cout << "On second " << num_seconds
-                  << ", robots look like:" << std::endl;
-        print_robot_positions(restroom_robots);
+
+        const double percentage_have_neighbors =
+            find_percentage_of_robot_positions_have_at_least_one_neighbor(
+                restroom_robots);
+        if(percentage_have_neighbors > NEIGHBOR_TOLERANCE) {
+            std::cout << "On second " << num_seconds
+                      << ", perentage of positions have neighbors: "
+                      << percentage_have_neighbors << std::endl;
+            std::cout << "Robots look like:" << std::endl;
+            print_robot_positions(restroom_robots);
+            break;
+        }
 
         num_seconds += 1;
     }
