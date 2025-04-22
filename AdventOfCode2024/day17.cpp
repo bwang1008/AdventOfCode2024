93d733b245c2f73aaafa9d6351dfe379cec7a12d
bwang1008 <bwang1008@gmail.com>
2025-04-22 01:22:47 -0500

    Finish day17B

--- AdventOfCode2024/day17.cpp
+++ AdventOfCode2024/day17.cpp
@@ -1,3 +1,53 @@
+/**
+Interpretation of given input program:
+2,4: B = A % 8
+1,2: B ^= 2
+7,5: C = A/(2 ^ B)
+4,5: B ^= C
+0,3: A = A/(2^3)
+1,7: B ^= 7
+5,5: print(B % 8)
+3,0: jump to first instruction again, until A is 0
+
+------------
+
+B = (A % 8) ^ 2
+B = B ^ (A / (2 ** B)) ^ 7
+print(B % 8)
+A /= 8
+go to beginning
+
+------------
+
+B = 5 ^ (A % 8) ^ (A / (2 ** ((A % 8) ^ 2)))
+print(B % 8)
+A /= 8
+go to beginning
+
+-----------
+
+So to get output of 2...
+if A % 8 == 7, then B = (2 ^ (A / 32)) which mod 8 is 2
+So (A/32) % 8 must be 0.
+
+if A % 8 == 6, then B = (3 ^ (A / 16)) which mod 8 is 2.
+So (A / 16) % 8 must be 1.
+
+if A % 8 == 5, then B = (A / 128) which mod 8 is 2.
+So (A / 128) % 8 must be 2.
+
+At the very least, can think about generating the digits backwards and build
+out register_a initial value from it.
+
+answer = 190384615275535
+How I generated my answer: had code search for what created the last 4 numbers: [5, 5, 3, 0]
+For instance, A = 2770.
+Then I checked which of [8A, 8A + 1, ..., 8A + 7] generated [7, 5, 5, 3, 0].
+I got A2 = 8A + 3 == 22163.
+Then I checked which of [8A2, 8A2 + 1, ..., 8A2 + 7] generated [1,7,5,5,3,0]
+and so on.
+*/
+
 #include <cstdint>   // std::size_t, int64_t
 #include <fstream>   // std::ifstream
 #include <iostream>  // std::cout, std::endl
@@ -10,7 +60,7 @@
 
 namespace Day17 {
 
-static auto parse_input()
+auto parse_input()
     -> std::tuple<std::vector<int>, int64_t, int64_t, int64_t> {
     const std::string input_file_name = "data/day17.txt";
     std::ifstream input_file{input_file_name};
@@ -213,6 +263,63 @@ class Computer {
     }
 };
 
+
+auto run_program(int64_t register_a) -> std::vector<int> {
+    std::vector<int> output;
+    
+    while(register_a != 0) {
+        int64_t register_b = (register_a % 8) ^ 2;
+        int64_t register_c = register_a;
+        for(int i = 0; i < register_b; ++i) {
+            register_c /= 2;
+        }
+        register_b ^= register_c ^ 7;
+        output.push_back(static_cast<int>(register_b % 8));
+        register_a /= 8;
+    }
+
+    return output;
+}
+
+/** 
+ * Return initial value of register_a such that run_program(register_a) results in 
+ * desired_output. Return -1 if not possible.
+ * 
+ * end of desired_output corresponds to most significant bits of register_a.
+ * So build up digits of register_a, starting from most significant down to least significant.
+ * We can do this because each digit printed by program is mainly decided by last 3 bits.
+ * 
+ * For instance, to build up [5, 5, 3, 0],
+ * Try A = 0, A = 1, ..., A = 7 such that run_program(A) results in [0].
+ * In this case, A = 5.
+ * Then try A = 8 * 5 + 0, A = 8 * 5 + 1, ..., 8 * 5 + 7 such that run_program(A) results in [3, 0]. 
+ * In this case, A = 8 * 5 + 3 = 43.
+ * Then try A = 8 * 43 + 0, A = 8 * 43 + 1, ..., 8 * 43 + 7 such that run_program(A) results in [5, 3, 0]
+ * and so on.
+ */
+auto get_desired_output(const std::vector<int> &desired_output) -> int64_t {
+    int64_t answer = 0;
+    const int division_of_a = 8;
+    for(std::vector<int>::const_reverse_iterator it = desired_output.crbegin(); it != desired_output.crend(); ++it) {
+        const int desired_digit = *it;
+        bool found_desired_digit = false;
+        for(int i = 0; i < division_of_a; ++i) {
+            const int64_t candidate = division_of_a * answer + i;
+            const std::vector<int> output = run_program(candidate);
+            if(!output.empty() && output[0] == desired_digit) {
+                answer = candidate;
+                found_desired_digit = true;
+                break;
+            }
+        }
+        if(!found_desired_digit) {
+            return -1;
+        }
+    }
+
+    return answer;
+}
+
 } // namespace Day17
 
 auto solve_day17a() -> int64_t {
@@ -239,84 +346,9 @@ auto solve_day17a() -> int64_t {
     return 0;
 }
 
-auto solve_day17b_old() -> int64_t {
-    const std::tuple<std::vector<int>, int64_t, int64_t, int64_t> inputs =
-        Day17::parse_input();
-    const std::vector<int> &program = std::get<0>(inputs);
-    const int64_t register_b = std::get<2>(inputs);
-    const int64_t register_c = std::get<3>(inputs);
-
-    bool found_identity = false;
-    int64_t register_a = 282050000;
-    register_a = 334000000;
-    register_a = 1130000000;
-
-    while(!found_identity) {
-        if(register_a % 2000000 == 0) {
-            std::cout << "Trying " << register_a << std::endl;
-        }
-        if(!(register_a % 8 == 7 && ((register_a / 32) % 8) == 0)) {
-            ++register_a;
-            continue;
-        }
-        Day17::Computer computer(register_a, register_b, register_c, true);
-        computer.run_program(program);
-        const std::vector<int> output = computer.get_output();
-        if(output == program) {
-            found_identity = true;
-        }
-        else {
-            ++register_a;
-        }
-    }
-
-    return register_a;
-}
-
 auto solve_day17b() -> int64_t {
     const std::tuple<std::vector<int>, int64_t, int64_t, int64_t> inputs =
         Day17::parse_input();
     const std::vector<int> &program = std::get<0>(inputs);
-
-    bool found_identity = false;
-    int64_t register_a = 282050000;
-    register_a = 334000000;
-    register_a = 1130000000;
-    register_a = 7150000000;
-    register_a = 281474976710656;
-
-    while(!found_identity) {
-        if(register_a % 50000000 == 0) {
-            std::cout << "Trying " << register_a << std::endl;
-        }
-        if(!(register_a % 8 == 7 && ((register_a / 32) % 8) == 0)) {
-            ++register_a;
-            continue;
-        }
-        
-        int64_t register_b = 0;
-        int64_t register_c = 0;
-        std::vector<int> output;
-        
-        int64_t working_a = register_a;
-        while(working_a != 0) {
-            register_b = (working_a % 8) ^ 2;
-            int64_t copy_a = working_a;
-            for(int i = 0; i < register_b; ++i) {
-                copy_a /= 2;
-            }
-            register_b = register_b ^ copy_a ^ 7;
-            output.push_back(register_b % 8);
-            working_a /= 8;
-        }
-
-        if(output == program) {
-            found_identity = true;
-        }
-        else {
-            ++register_a;
-        }
-    }
-
-    return register_a;
+    return Day17::get_desired_output(program);
 }
